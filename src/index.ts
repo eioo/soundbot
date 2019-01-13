5c27945fd385a944d8e036b732a31fb64a458105
Kallu609 <khirvola@gmail.com>
2019-01-14 00:37:28 +0200

    fixed import

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import * as dotenv from 'dotenv';
-import * as Logger from 'util';
+import * as Logger from './utils/logger';
 
 dotenv.config();
 process.env.NTBA_FIX_319 = '😋'; // Gets rid of node-telegram-bot-api warnings
 process.env.NTBA_FIX_350 = '😋'; // -//-
 
 process.on('unhandledRejection', e => {
-  Logger.log(e);
+  Logger.error(e);
 });
 
 import { startBot } from './bot';
